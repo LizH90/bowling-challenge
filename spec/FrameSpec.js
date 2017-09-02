bbe2ce3537aa486eb94b193bc266db842fada8cc
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-09-02 13:23:20 +0100

    added 2nd roll function

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -13,10 +13,19 @@ describe("Frame", function() {
   describe("when calculating rolls", function() {
     beforeEach(function() {
       frame.calculateroll1();
+      frame.calculateroll2();
     });
 
     it("should add roll to the array", function() {
       expect(frame.rolls).not.toEqual([]);
+    });
+
+    it("reduces the amount of pins available", function () {
+      expect(frame.pins).toEqual(10-frame.rolls[0]);
+    });
+
+    it("should add a second element to the array", function() {
+      expect(frame.rolls[1]).not.toEqual('undefined');
     });
   });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,8 +1,15 @@
 function Frame() {
   this.rolls = [];
+  this.pins = 10
 };
 
 Frame.prototype.calculateroll1 = function() {
   var roll1 = (Math.floor(Math.random()*11));
   this.rolls.push(roll1);
+  this.pins = (10 - roll1);
+};
+
+Frame.prototype.calculateroll2 = function() {
+  var roll2 = (Math.floor(Math.random()*(11-this.rolls)));
+  this.rolls.push(roll2);
 };
